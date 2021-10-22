fba1a0ca75db90d02a5e66a6f4bc121403468eb7
alexander coronel <alexithocoronel@gmail.com>
2021-10-22 17:02:06 -0500

    primer repositorio git

---
+++ taller2prueba1/src/taller2prueba1/Taller2prueba1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package taller2prueba1;
+
+/**
+ *
+ * @author Alexander Coronel
+ */
+public class Taller2prueba1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("taller 2 introducion a la programacion git hub");
+        // TODO code application logic here
+    }
+    
+}
